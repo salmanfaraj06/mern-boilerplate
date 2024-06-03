6206defa1c72296e75e68ffc602e48bd9ee299e0
salmanfaraj06 <salman.faraj06@gmail.com>
2024-06-03 20:52:30 +0530

    added initial client files

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
