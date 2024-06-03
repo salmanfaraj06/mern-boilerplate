06b9518d159276be6398fb6e0825c6607b941417
salmanfaraj06 <salman.faraj06@gmail.com>
2024-06-03 20:52:52 +0530

    added initial backend setup

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+// import cors from 'cors';
+
+const app = express();
+
+app.listen(5000, () => {
+  console.log("Server started at port 5000");
+});
+
+mongoose.connect(process.env.MONGODB_CONNECTION)
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log('MongoDB connection error:', err));
