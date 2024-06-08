c7f6d48bf49f34714a01738f7d99178aef5825bb
salmanfaraj06 <salman.faraj06@gmail.com>
2024-06-08 16:01:06 +0530

    Add rate limiter for login endpoint

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -2,71 +2,77 @@
 import User from "../models/user.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import rateLimit from 'express-rate-limit';
+import rateLimit from "express-rate-limit";
 
 export const register = async (req, res, next) => {
-    const { username, email, password } = req.body;
+  const { username, email, password } = req.body;
 
-    try {
-        const existingUser = await User.findOne({ email });
+  try {
+    const existingUser = await User.findOne({ email });
 
-        if (existingUser) {
-            return res.status(400).json({ message: "User already exists" });
-        }
-
-        const hashedPassword = bcrypt.hashSync(password, 12);
-
-        const result = await User.create({
-            email,
-            username,
-            password: hashedPassword,
-        });
-
-        const token = jwt.sign(
-            { email: result.email, id: result._id },
-            process.env.JWT_SECRET || "test",
-            { expiresIn: "1h" }
-        );
-
-        res.status(200).json({ result, token });
-        console.log("User registered successfully");
-    } catch (error) {
-        next(error); // Passes the error to the error-handling middleware
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
     }
-}
+
+    const hashedPassword = bcrypt.hashSync(password, 12);
+
+    const result = await User.create({
+      email,
+      username,
+      password: hashedPassword,
+    });
+
+    const token = jwt.sign(
+      { email: result.email, id: result._id },
+      process.env.JWT_SECRET || "test",
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({ result, token });
+    console.log("User registered successfully");
+  } catch (error) {
+    next(error); // Passes the error to the error-handling middleware
+  }
+};
 
 const loginLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-    message: "Too many login attempts from this IP, please try again after 15 minutes"
-  });
-  
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message:
+    "Too many login attempts from this IP, please try again after 15 minutes",
+});
 
-  export const login = [loginLimiter, async (req, res, next) => {
+export const login = [
+  loginLimiter,
+  async (req, res, next) => {
     const { email, password } = req.body;
-  
+
     try {
       const existingUser = await User.findOne({ email });
-  
+
       if (!existingUser) {
         return res.status(404).json({ message: "User doesn't exist" });
       }
-  
-      const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
-  
+
+      const isPasswordCorrect = await bcrypt.compare(
+        password,
+        existingUser.password
+      );
+
       if (!isPasswordCorrect) {
-        return res.status(400).json({ message: "Invalid credentials" });
+        return res.status(401).json({ message: "Invalid credentials" });
       }
-  
+
       const token = jwt.sign(
         { email: existingUser.email, id: existingUser._id },
         process.env.JWT_SECRET,
         { expiresIn: "1h" }
       );
-  
+
       res.status(200).json({ result: existingUser, token });
       console.log("User logged in successfully");
     } catch (error) {
-        next(error); 
+      next(error);
     }
-  }];
+  },
+];
