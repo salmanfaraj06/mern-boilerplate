74c43a5704bbbd30d8e73c3fcd9a918fa3dbcbb9
salmanfaraj06 <salman.faraj06@gmail.com>
2024-06-14 22:30:17 +0530

    Update Home component layout for responsiveness

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
 
   return (
     <div className={darkMode ? "dark" : ""}>
-      <div className="bg-white dark:bg-black text-black dark:text-white min-h-screen flex flex-col items-center justify-center">
+      <div className="bg-white dark:bg-black text-black dark:text-white min-h-screen mx-auto max-w-6xl flex flex-col items-center justify-center">
         <h1 className="text-4xl mb-4">Welcome to Our MERN Starter Kit</h1>
         <p className="mb-4">
           This is a fully responsive landing page for a MERN stack application.
